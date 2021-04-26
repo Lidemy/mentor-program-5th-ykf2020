074038f5cc540ae660c378def1d650a419e7db6a
Kun <yukun0620@gmail.com>
2021-04-26 09:52:35 +0800

    finished week2

--- homeworks/week2/hw2.js
+++ homeworks/week2/hw2.js
@@ -1,5 +1,10 @@
-function capitalize(str) {
-  
+function capitalize(str){
+  var strsplit = str.split('')
+
+  if(strsplit[0]>="a" && strsplit[0] <= "z" ){
+    strsplit[0] = strsplit[0].toUpperCase()
+  }
+  return(strsplit.join(""))
 }
 
 console.log(capitalize('hello'));
